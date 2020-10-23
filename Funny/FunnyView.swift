d053eb9ac04bc6ec229defaf2e1ae0c929f56be0
yanzhen <yanzhen@meishubao.com>
2020-10-23 14:49:35 +0800

    bg color

--- Funny/FunnyView.swift
+++ Funny/FunnyView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct FunnyView: View {
     var body: some View {
-        let apps = [("note", "办公家具"), ("note", "办公家具"), ("", ""), ("", "")]
-        ScrollView(.vertical) {
-            LazyHGrid(rows: Array(repeating: GridItem(.fixed(85)), count: 1), spacing: (SCREENW - 240)/5) {
-                ForEach(0..<apps.count) { index in
-                    FunnyAppView(image: apps[index].0, title: apps[index].1)
-                        .frame(width: 60)
+        let apps = [("note", "办公"), ("note", "家具"), ("", ""), ("", "")]
+        NavigationView {
+            ScrollView(.vertical) {
+                LazyHGrid(rows: Array(repeating: GridItem(.fixed(85)), count: 1), spacing: (SCREENW - 240)/5) {
+                    ForEach(0..<apps.count) { index in
+                        FunnyAppView(image: apps[index].0, title: apps[index].1)
+                            .frame(width: 60)
+                    }
                 }
-            }
-            .padding()
+                .padding()
+            }//can not adjust the order
+            .frame(width: SCREENW)
+            .background(Color(red: 242/255.0, green: 242/255.0, blue: 247/255.0, opacity: 1))
+            .navigationBarTitle("Funny", displayMode: .inline)
+            .ignoresSafeArea(edges: .bottom)
         }
     }
 }
---
+++ Funny/Main/FunnyBGView.swift
@@ -0,0 +1,21 @@
+//
+//  FunnyBGView.swift
+//  Funny
+//
+//  Created by yanzhen on 2020/10/23.
+//
+
+import SwiftUI
+
+struct FunnyBGView: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.blue)
+    }
+}
+
+struct FunnyBGView_Previews: PreviewProvider {
+    static var previews: some View {
+        FunnyBGView()
+    }
+}
